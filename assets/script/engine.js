62d8abcf6c34ef8dc4c6fa24fad1d39a3e948407
SpyHenry <henrybbandeira@gmail.com>
2025-10-07 01:57:32 -0300

    Meu Portifólio

--- assets/script/engine.js
+++ assets/script/engine.js
@@ -1,18 +1,35 @@
 // Função de troca de tema claro/escuro
 
-const themeToggle = document.getElementById('theme-toggle');
-const rootHtml = document.documentElement;
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
 
-function changeTheme() {
-    const currentTheme = rootHtml.getAttribute('data-theme');
+function changeTheme(){
+  const currentTheme = rootHtml.getAttribute("data-theme");
 
-    if (currentTheme === 'dark') rootHtml.setAttribute('data-theme', 'light');
-    else rootHtml.setAttribute('data-theme', 'dark');
+  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
 
-    themeToggle.classList.toggle('bi-sun');
-    themeToggle.classList.toggle('bi-moon');
+  toggleTheme.classList.toggle("bi-sun")
+  toggleTheme.classList.toggle("bi-moon-stars")
 }
 
-themeToggle.addEventListener('click', changeTheme);
+toggleTheme.addEventListener("click", changeTheme);
+
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+  })
+})
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  })
+})
 
 // --------------------------
